0ed112322ed7f367cace6f1a8ba405c88c1932ce
nayan <jhanayan543@gmail.com>
2024-05-27 12:15:13 +0530

    First Commit

---
+++ src/components/Leaderboard.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Data from '../Utills/MOCK_DATA'
+
+function Leaderboard() {
+  
+  return (
+    <div className="text-3xl font-bold underline">
+        <div>
+            header
+        </div>
+        <div>
+            <div className=" border-[2px]"><></></div>
+            <div>c2</div>
+        </div>
+    </div>
+  )
+}
+
+export default Leaderboard
